9f8a032d37607aec3972be035bd27442c5e5641c
yunyun <yunyuntest@qq.com>
2023-05-25 22:26:16 +0800

    添加基础
    Signed-off-by: yunyun <yunyuntest@qq.com>

---
+++ style/js/mapmate.js
@@ -0,0 +1,18 @@
+class Mapmate {
+
+
+    /**
+     * 构造函数
+     *
+     * @param ele 元素ID
+     * @param options 入参
+     */
+    constructor(ele, options) {
+        this.mapContainer = document.getElementById(ele);
+        this.options = options;
+        this.maps = {};
+    }
+
+
+
+}
